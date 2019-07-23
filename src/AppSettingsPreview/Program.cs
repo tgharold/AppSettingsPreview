60d1445573c5d40e7c649c56464ae6844c0a8b28
Thomas Harold <tgharold@gmail.com>
2019-07-23 07:34:41 -0400

    create solution/project

---
+++ src/AppSettingsPreview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppSettingsPreview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
