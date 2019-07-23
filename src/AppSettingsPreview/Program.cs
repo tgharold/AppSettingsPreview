bfe7e4d130d4623ca72f680d1d92a4acb1ce0567
Thomas Harold <tgharold@gmail.com>
2019-07-23 07:51:49 -0400

    add some notes and reference URLs

--- src/AppSettingsPreview/Program.cs
+++ src/AppSettingsPreview/Program.cs
@@ -6,7 +6,26 @@ namespace AppSettingsPreview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO: check argument count
+
+            var baseName = GetSettingsFileName();
+            var compareToName = GetSettingsFileName(args[0]);
+
+            //TODO: Read in JSON strings from files
+            //TODO: sort/deserialize
+            //TODO: compare
+            //TODO: output
+        }
+
+        // https://stackoverflow.com/a/56185448/727286
+        // https://github.com/wbish/jsondiffpatch.net
+
+        private static string GetSettingsFileName(string environment = null)
+        {
+            environment = environment ?? string.Empty;
+            if (environment.Length > 0)
+                environment = $".{environment}";
+            return $"appsettings{environment}.json";
         }
     }
 }
